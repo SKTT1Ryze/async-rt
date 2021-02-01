daa2f9e16216b1aaf712426499bfc326d081265a
hustccc <1276675421@qq.com>
2021-02-02 07:14:59 +0900

    fix something

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! A Simple Implementation of Rust `Async` Runtime
 //! 
+//! Ref: https://github.com/richardanaya/executor
+//! 
 //! This crate is mainly for `Rust embedded` project  
 //! 
 //! Make sure initializing the `Heap Allocator` before use the `Executor`
@@ -29,13 +31,13 @@ use {
 /// Executor holds a queue of tasks that ready to make more progress
 #[derive(Default)]
 pub struct Executor {
-    tasks: Mutex<VecDeque<Box<dyn IsTask + core::marker::Send + core::marker::Sync>>>
+    tasks: VecDeque<Box<dyn IsTask + core::marker::Send + core::marker::Sync>>
 }
 
 /// Reactor holds a queue of tasks that pending
 #[derive(Default)]
 pub struct Reactor {
-    tasks: Mutex<Vec<Box<dyn IsTask + core::marker::Sync + core::marker::Send>>>
+    tasks: Vec<Box<dyn IsTask + core::marker::Sync + core::marker::Send>>
 }
 
 trait IsTask: HasID + TaskPoll {}
@@ -79,11 +81,11 @@ impl<T> IsTask for Arc<Task<T>> {}
 impl<T> Woke for Task<T> {
     fn wake_by_ref(arc_self: &Arc<Self>) {
         // move the task from Reactor queue to Executor queue
-        while let Some(task) = DEFAULT_REACTOR.tasks.lock().pop() {
+        while let Some(task) = DEFAULT_REACTOR.lock().tasks.pop() {
             if task.id() == arc_self.id() {
-                DEFAULT_EXECUTOR.tasks.lock().push_back(task);
+                DEFAULT_EXECUTOR.lock().tasks.push_back(task);
             } else {
-                DEFAULT_REACTOR.tasks.lock().push(task);
+                DEFAULT_REACTOR.lock().tasks.push(task);
             }
         }
     }
@@ -110,6 +112,15 @@ impl Executor {
         }
     }
 
+    // Run async task but don't block
+    pub fn run<T>(&mut self, future: Box<dyn Future<Output = T> + 'static + Send + Unpin>)
+    where
+        T: Send + 'static,
+    {
+        self.append_task(future);
+        self.poll_tasks();
+    }
+
     /// Add a future as Task to the queue of tasks for the first poll
     fn append_task<T>(
         &mut self,
@@ -123,23 +134,23 @@ impl Executor {
             future: Mutex::new(Box::pin(future)),
             id: unsafe { let id = COUNTER; COUNTER += 1; id}
         });
-        self.tasks.lock().push_back(Box::new(task.clone()));
+        self.tasks.push_back(Box::new(task.clone()));
         task
     }
 
     // Poll all the tasks in executor queue
     fn poll_tasks(&mut self) {
-        while let Some(task) = self.tasks.lock().pop_front() {
+        while let Some(task) = self.tasks.pop_front() {
             if !task.task_poll() {
                 // If task not ready, push it to Reactor queue
-                DEFAULT_REACTOR.tasks.lock().push(task);
+                DEFAULT_REACTOR.lock().tasks.push(task);
             }
         }
     }
 }
 
 impl Reactor {
-    fn append_task<T>(
+    fn _append_task<T>(
         &mut self,
         future: Box<dyn Future<Output = T> + 'static + Send + Unpin>
     ) -> Arc<Task<T>>
@@ -151,20 +162,35 @@ impl Reactor {
             future: Mutex::new(Box::pin(future)),
             id: unsafe { let id = COUNTER; COUNTER += 1; id}
         });
-        self.tasks.lock().push(Box::new(task.clone()));
+        self.tasks.push(Box::new(task.clone()));
         task
     }
 }
 
 lazy_static! {
-    static ref DEFAULT_EXECUTOR: Box<Executor> = {
+    static ref DEFAULT_EXECUTOR: Mutex<Box<Executor>> = {
         let e = Executor::default();
-        Box::new(e)
+        Mutex::new(Box::new(e))
     };
-    static ref DEFAULT_REACTOR: Box<Reactor> = {
+    static ref DEFAULT_REACTOR: Mutex<Box<Reactor>> = {
         let r = Reactor::default();
-        Box::new(r)
+        Mutex::new(Box::new(r))
     };
 }
 
 static mut COUNTER: usize = 0;
+
+/// Give future to global executor to be polled and executed.
+pub fn block_on<T>(future: impl Future<Output = T> + 'static + Send) -> T
+where
+    T: Send + 'static,
+{
+    DEFAULT_EXECUTOR.lock().block(Box::new(Box::pin(future)))
+}
+
+pub fn run<T>(future: impl Future<Output = T> + 'static + Send)
+where
+    T: Send + 'static,
+{
+    DEFAULT_EXECUTOR.lock().run(Box::new(Box::pin(future)))
+}
